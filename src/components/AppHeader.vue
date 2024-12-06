e46adfe87b4ee29d5f76f7aab3c4417d8036b3bc
DanieleChecchin <danielechecchin26@gmail.com>
2024-12-06 12:23:09 +0100

    Fixed header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,46 +12,44 @@ export default {
 <template>
   <header>
     <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid">
+      <div class="container d-flex justify-content-between align-items-center">
+
+        <figure>
+          <img src="/public/images/BoolBnb.png" alt="logo BoolBnB" id="logo-header">
+        </figure>
         <div class="collapse navbar-collapse d-flex justify-content-center align-items-center" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <img src="/public/images/BoolBnb.png" alt="logo BoolBnB" id="logo-header">
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link fw-bold fs-5" aria-current="page" :to="{ 'name': 'homepage' }">
+              <router-link class="nav-link fw-bold fs-4" aria-current="page" :to="{ 'name': 'homepage' }">
                 Home
               </router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link fw-bold fs-5 mx-3" aria-current="page" :to="{ 'name': 'about' }">
+              <router-link class="nav-link fw-bold fs-4 mx-3" aria-current="page" :to="{ 'name': 'about' }">
                 About
               </router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link fw-bold fs-5" aria-current="page" :to="{ 'name': 'properties' }">
+              <router-link class="nav-link fw-bold fs-4" aria-current="page" :to="{ 'name': 'properties' }">
                 Properties List
               </router-link>
             </li>
           </ul>
         </div>
+        <div class="right-header d-flex justify-content-center align-items-center">
+          <h5>
+            Rent with BnB
+          </h5>
+        </div>
+
       </div>
     </nav>
   </header>
 </template>
 
 <style scoped>
-header {
-  background-color: #B7C4D8;
-  position: relative;
-  height: 80px;
-}
-
 #logo-header {
   width: 80px;
   filter: drop-shadow(0 4px 6px rgba(0, 0, 0, 0.2));
-  position: absolute;
-  left: 10px;
-  top: 5px;
 }
 </style>
