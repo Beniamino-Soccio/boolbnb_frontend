941b5b4dfd1dc472e82642bc1900d78f962fa1cd
DanieleChecchin <danielechecchin26@gmail.com>
2024-12-06 11:57:51 +0100

    fixed header layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,22 +11,25 @@ export default {
 
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg">
       <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse d-flex justify-content-center align-items-center" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <router-link class="nav-link fw-bold fs-4" aria-current="page" :to="{ 'name': 'homepage' }">
+              <img src="/public/images/BoolBnb.png" alt="logo BoolBnB" id="logo-header">
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link fw-bold fs-5" aria-current="page" :to="{ 'name': 'homepage' }">
                 Home
               </router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link fw-bold fs-4" aria-current="page" :to="{ 'name': 'about' }">
+              <router-link class="nav-link fw-bold fs-5 mx-3" aria-current="page" :to="{ 'name': 'about' }">
                 About
               </router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link fw-bold fs-4" aria-current="page" :to="{ 'name': 'properties' }">
+              <router-link class="nav-link fw-bold fs-5" aria-current="page" :to="{ 'name': 'properties' }">
                 Properties List
               </router-link>
             </li>
@@ -37,4 +40,18 @@ export default {
   </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  background-color: #B7C4D8;
+  position: relative;
+  height: 80px;
+}
+
+#logo-header {
+  width: 80px;
+  filter: drop-shadow(0 4px 6px rgba(0, 0, 0, 0.2));
+  position: absolute;
+  left: 10px;
+  top: 5px;
+}
+</style>
