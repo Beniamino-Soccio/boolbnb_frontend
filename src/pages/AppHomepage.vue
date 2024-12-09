5650193586e58a58e49c9f5f38e6a94253b606f8
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-12-09 12:03:50 +0100

    search property from all in a radius from the place searched

--- src/pages/AppHomepage.vue
+++ src/pages/AppHomepage.vue
@@ -1,8 +1,35 @@
 <script>
+import axios from 'axios';
+import { store } from '../js/store.js'
+
+import BasicFilter from '../components/BasicFilter.vue';
+
 export default {
   data() {
     return {
+      store,
+    }
+  },
+  components: {
+    BasicFilter
+  },
+  methods: {
+    sendSearchParametres() {
+      const formData = {
+        latitude: store.latitude,
+        longitude: store.longitude,
+        radius: store.radius
+      }
 
+      axios.post(store.apiUrl, formData)
+        .then((response) => {
+          console.log(response.data.result);
+          store.property = response.data.result;
+          store.searchedBool = true;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
   }
 }
@@ -15,6 +42,7 @@ export default {
       <div class="col-12">
         <h1 class="fw-bold text-center">Home Page BoolBnb</h1>
       </div>
+      <BasicFilter @propertyCall="sendSearchParametres" />
       <div class="col-12 py-5 text-center">
         <img src="/public/images/BoolBnb.png" alt="Logo BoolBnb">
       </div>
