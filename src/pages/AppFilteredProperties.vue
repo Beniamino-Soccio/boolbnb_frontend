0b4d0f751901066131410893f5875f890d1f2775
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-12-11 17:23:21 +0100

    call filter in filteddered properties

--- src/pages/AppFilteredProperties.vue
+++ src/pages/AppFilteredProperties.vue
@@ -1,16 +1,45 @@
 <script>
+import axios from 'axios';
+import { store } from '../js/store.js'
 
+import BasicFilter from '../components/BasicFilter.vue';
 import FilteredPropertiesList from '../components/FilteredPropertiesList.vue';
 
 export default {
     name: 'AppProperties',
     data() {
         return {
-
+            store,
         }
     },
     components: {
         FilteredPropertiesList,
+        BasicFilter
+    },
+    methods: {
+        sendSearchParametres() {
+            const formData = {
+                latitude: store.latitude,
+                longitude: store.longitude,
+                radius: store.radius
+            }
+
+            axios.post(store.apiUrl, formData)
+                .then((response) => {
+                    store.searchedBool = false;
+                    console.log(response.data.result);
+                    store.property = response.data.result;
+                    if (response.data.result.length > 0) {
+                        store.searchedBool = true;
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    },
+    created() {
+        console.log("sono il router:", this.$router.currentRoute)
     }
 }
 </script>
@@ -22,6 +51,7 @@ export default {
             <div class="col-12">
                 <h1 class="fw-bold my-5 text-center"> Searched Properties List:</h1>
             </div>
+            <BasicFilter @propertyCall="sendSearchParametres" class="mb-5" />
             <FilteredPropertiesList :withShow="true" />
         </div>
     </div>
